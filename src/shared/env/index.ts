dda1615e497d26e00edb4e740d2b6939d5c3791c
EdsonOttoni <edson.ottoni@gmail.com>
2023-05-01 16:12:56 -0300

    corrigindo sintaxe

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -10,6 +10,7 @@ const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.log(_env.error.format())
-  throw new Error(`Invalid evironment variables `)
+  throw new Error(`Invalid environment variables `)
 }
+
 export const env = _env.data

e14f3fa912001a26f3f2392d62412be2618a2f2d
EdsonOttoni <edson.ottoni@gmail.com>
2023-05-17 11:42:26 -0300

    Outros controllers da academia

--- src/http/routes/gyms-routes.ts
+++ src/http/routes/gyms-routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
+import { create } from '../controllers/gyms/create'
+import { nearby } from '../controllers/gyms/nearby'
+import { search } from '../controllers/gyms/search'
 import { verifyJwt } from '../middlewares/verify-jwt'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.post('/gyms', create)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }

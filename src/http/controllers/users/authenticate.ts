b23698085f94b4d1c944c6c31d23c45ff97ca6fb
EdsonOttoni <edson.ottoni@gmail.com>
2023-05-20 14:12:53 -0300

    Implementação do refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -30,7 +30,26 @@ export async function authenticate(
         },
       }
     )
-    return reply.status(200).send({ token })
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      }
+    )
+
+    return reply
+      .status(200)
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .send({ token })
   } catch (err) {
     if (err instanceof InvalidCredentialError)
       return reply.status(400).send({ error: err.message })
--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -1,5 +1,6 @@
 import { authenticate } from '@/http/controllers/users/authenticate'
 import { profile } from '@/http/controllers/users/profile'
+import { refresh } from '@/http/controllers/users/refresh'
 import { register } from '@/http/controllers/users/register'
 import { FastifyInstance } from 'fastify'
 
@@ -8,6 +9,8 @@ import { verifyJwt } from '../middlewares/verify-jwt'
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticate */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
